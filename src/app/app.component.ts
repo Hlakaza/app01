072ad66f0fd593c00d34f9233b06bbb9646be82a
hlakaza <nhlalucky9@gmail.com>
2019-07-28 22:11:07 +0200

    feat(init): initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'app01';
+
+  cartNumber = 55;
+
+  nav = [
+    { name: 'Home', link: '', },
+    { name: 'Shop Brands', link: '', },
+    { name: 'Shop Brands', link: '', },
+  ];
+
+  footerav = {
+       headers: {
+         header1: 'Shop Top Brands',
+         header2: 'Skin Shop'
+       },
+       topBrands: [
+         { name: 'Shop Brands', link: '', },
+         { name: 'Shop Brands', link: '', },
+        ],
+        skinShop: [
+         { name: 'Shop Brands', link: '', },
+         { name: 'Shop Brands', link: '', },
+        ],
+  };
+
+
 }
